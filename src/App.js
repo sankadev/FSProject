67bc136c209f2fa789727822d2d6277644a438bd
Vidura Wickramaarachchi <vidura@rightapps.com.au>
2022-03-04 14:13:35 +0530

    final changes before demo

--- src/App.js
+++ src/App.js
@@ -4,63 +4,50 @@ import { db } from './firebase'
 import { collection, getDoc, getDocs, doc } from 'firebase/firestore'
 import DataCard from './Components/dataCard'
 import UserCard from './Components/userCard'
-import axios from "axios";
 import {
   BrowserRouter as Router,
   useParams,
 } from "react-router-dom";
-
+import base64 from 'react-native-base64'
 
 
 //import Home from "./index";
 
 function App() {
   let { deviceID } = useParams();
-  // const { id } = props.match.params;
   const [data, setData] = useState([])
 
-  var decodedStringAtoB = atob(deviceID);
+  const getFirestoreData = async () => {
+    console.log("Run firestore");
+    const decodedDeviceID = atob(deviceID);
 
-
-  // collection ref- replace 'users' with 'your firebase collection'
-  const dataRef = collection(db, 'Sensor')
-
-  // get data from firestore
-  const getNotes = async () => {
-    const dataSnapshot = await getDocs(dataRef)
-    const dataList = dataSnapshot.docs.map(doc => doc.data())
-    // if the user preset pass auth i data id here
-    return setData(dataList[1])
-  }
-
-  const getfirestoreData = async () => {
-    console.log("Run firestore")
-    const docRef = doc(db, "newest_data", decodedStringAtoB);
+    console.log('Fetching data for id', decodedDeviceID);
+    const docRef = doc(db, "newest_data", decodedDeviceID);
     const docSnap = await getDoc(docRef);
 
-
+    console.log('found document', docSnap.data());
     if (docSnap.exists()) {
       console.log("Document data:", docSnap.data());
-
       return setData(docSnap.data());
     } else {
       // doc.data() will be undefined in this case
       console.log("No such document!");
     }
-
   }
 
   // fetch the data at initial render
   useEffect(() => {
+    const interval = setInterval(() => {
+      // Only fetch the data from firebase if the deviceID is not empty
+      if(deviceID) {
+        getFirestoreData();
+      }
+    }, 3000);
+    return () => clearInterval(interval);
 
-    //getNotes()
-    getfirestoreData()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   console.log(data, 'data')
-
-
 
 
   return (
@@ -70,7 +57,8 @@ function App() {
 
       <div class="row">
         <div class="col-sm">
-          <UserCard device={decodedStringAtoB} />
+          {deviceID && <UserCard deviceID={deviceID} />}
+          {!deviceID && <p>No user is specified</p>}
         </div>
         <div class="col-sm">
         </div>
@@ -80,12 +68,12 @@ function App() {
       </div>
 
 
-      <DataCard
-        heartRate={data.pulserate}
-        temp={data.bodytemp}
-        spo={data.spo}
-        movement={data.movement}
-      />
+      {data && <DataCard
+        heartRate={data?.pulserate}
+        temp={data?.bodytemp}
+        spo={data?.spo}
+        movement={data?.movement}
+      />}
 
     </div>
 
@@ -94,3 +82,26 @@ function App() {
 }
 
 export default App
+
+/*
+
+  //var decodedStringAtoB = Buffer.from(deviceID, 'base64').toString('ascii')
+  //let decodedStringAtoB = base64.decode(deviceID);
+  let decodedStringAtoB = base64.decode(deviceID);
+
+
+  // // collection ref- replace 'users' with 'your firebase collection'
+  // const dataRef = collection(db, 'Sensor')
+
+  // // get data from firestore
+  // const getNotes = async () => {
+  //   const dataSnapshot = await getDocs(dataRef)
+  //   const dataList = dataSnapshot.docs.map(doc => doc.data())
+  //   // if the user preset pass auth i data id here
+  //   return setData(dataList[1])
+  // }
+
+
+
+
+*/
